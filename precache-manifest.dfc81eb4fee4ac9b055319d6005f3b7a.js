fe48f0e0680fe84d186154f95997c99f0e1f04aa
Daniel Pôrto Núñez <daniel@MacBook-Pro-de-Daniel.local>
2020-05-22 10:39:38 -0300

    Updates

---
+++ precache-manifest.dfc81eb4fee4ac9b055319d6005f3b7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d281ff8a802ec5106980de5f5625e62",
+    "url": "/dpn-page/index.html"
+  },
+  {
+    "revision": "79ef707f3ebf649e10a2",
+    "url": "/dpn-page/static/js/2.100e9861.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dpn-page/static/js/2.100e9861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079009f7fd54b8bba89c",
+    "url": "/dpn-page/static/js/main.733cb274.chunk.js"
+  },
+  {
+    "revision": "5b9513b141c35bef1912",
+    "url": "/dpn-page/static/js/runtime-main.821c1bea.js"
+  }
+]);
